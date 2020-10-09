cecf4607eb7502b8b8affb8631badf20e01cbd4b
Amy Jiang <amyjiang@Amys-MBP.fios-router.home>
2020-10-08 21:26:17 -0400

    Sentence and word tokenizer in tokenizerfunction.py

---
+++ textsummarizer/tokenizerfunction.py
@@ -0,0 +1,13 @@
+import nltk
+
+text="Mary had a little lamb. Her fleece was white as snow"
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+sents=sent_tokenize(text)
+print(sents)
+
+words=[word_tokenize(sent) for sent in sents]
+print(words)
+
+
+
