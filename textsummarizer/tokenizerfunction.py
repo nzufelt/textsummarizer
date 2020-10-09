759baa19f01c075430f783c1c7945b74a54ca7f9
Amy Jiang <amyjiang@Amys-MBP.fios-router.home>
2020-10-08 21:26:47 -0400

    Removing stopwords from tokenizerfunction.py

--- textsummarizer/tokenizerfunction.py
+++ textsummarizer/tokenizerfunction.py
@@ -9,5 +9,9 @@ print(sents)
 words=[word_tokenize(sent) for sent in sents]
 print(words)
 
-
+from nltk.corpus import stopwords
+from string import punctuation
+customStopWords=set(stopwords.words('english')+list(punctuation))
+wordsWOStopwords=[word for word in word_tokenize(text) if word not in customStopWords]
+print(wordsWOStopwords)
 
