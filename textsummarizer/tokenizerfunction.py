0293363fa919877af2751716abd9e47f03c47b15
Amy Jiang <amyjiang@Amys-MBP.fios-router.home>
2020-10-08 21:40:06 -0400

    Allow user to enter their own text input to tokenizerfunction.py

--- textsummarizer/tokenizerfunction.py
+++ textsummarizer/tokenizerfunction.py
@@ -1,6 +1,6 @@
 import nltk
 
-text="Mary had a little lamb. Her fleece was white as snow"
+text = input ("Enter your text here: ")
 from nltk.tokenize import word_tokenize, sent_tokenize
 
 sents=sent_tokenize(text)
