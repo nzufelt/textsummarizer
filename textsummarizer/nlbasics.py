097a205a3d65fa48e12b8a3c7b34877f68e14479
Amy Jiang <amyjiang@Amys-MBP.fios-router.home>
2020-10-14 12:37:32 -0400

    Reorganizing code by moving the basic modules into nlbasics.py

---
+++ textsummarizer/nlbasics.py
@@ -0,0 +1,21 @@
+import nltk
+
+text = input ("Enter your text here: ")
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+sents=sent_tokenize(text)
+print(sents)
+
+words=[word_tokenize(sent) for sent in sents]
+print(words)
+
+from nltk.corpus import stopwords
+from string import punctuation
+customStopWords=set(stopwords.words('english')+list(punctuation))
+wordsWOStopwords=[word for word in word_tokenize(text) if word not in customStopWords]
+print(wordsWOStopwords)
+
+from nltk.collocations import *
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+finder = BigramCollocationFinder.from_words(wordsWOStopwords)
+print(finder.ngram_fd.items())
